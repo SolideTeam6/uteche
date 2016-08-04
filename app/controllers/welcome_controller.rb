b3512055456c429930d9d4fd82a3f693fbb78b90
Samuel Herrera <sherrera@solidemx.com>
2016-08-03 23:26:02 -0600

    vista alumno

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,9 @@ class WelcomeController < ApplicationController
         @lastVentas = Ventum.all.order('created_at desc').limit(10)
         @stocks =  Stock.where("cantidad < 10")
 
+        @venta = Ventum.all.order('created_at desc')
+
+
+        @deposito = Deposito.all.order('created_at desc')
     end
 end
